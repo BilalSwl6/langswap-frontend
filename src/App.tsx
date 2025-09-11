1ce849c8844d50dc959bfc1cea7387d7b33a5459
mbilal2913 <mbilal2913@gmail.com>
2025-09-11 22:50:17 +0000

    initial react setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center mt-4'>
+        <Button>Test Button</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
